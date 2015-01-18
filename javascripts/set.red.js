381b93a54be2cc1e0a6b8632f3fbf582513d9c59
Nicolas Degen <nicolas.degen@gmail.com>
2015-01-18 21:56:05 +0100

    edits

--- javascripts/set.red.js
+++ javascripts/set.red.js
@@ -1,8 +1,21 @@
 function setRedColorScheme() {
-  document.body.style.background = "#990012";
+  document.body.style.background = "#6f0000";
 
-  var h1s = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, a, li");
+  var h1s = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p");
   for(var i = 0; i < h1s.length; i++) {
-    h1s[i].style.color = "#ffffff";
+    h1s[i].style.color = "#fff";
+  }
+  var as = document.querySelectorAll("li, a");
+  for(var i = 0; i < as.length; i++) {
+    as[i].style.color = "#000";
+  }
+}
+
+function fadeOutRedColorScheme() {
+  document.body.style.background = "#fff";
+
+  var h1s = document.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li");
+  for(var i = 0; i < h1s.length; i++) {
+    h1s[i].style.color = "#000";
   }
 }
